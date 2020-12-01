97761ff11d3b2579a5731a487d367a545c9c711f
Gomathy <kgomathy2001@gmail.com>
2020-12-01 18:47:41 +0530

    added patient details

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -5,11 +5,15 @@ DROP TABLE IF EXISTS patient;
 
 
 CREATE TABLE patient(
-   id INTEGER PRIMARY KEY AUTOINCREMENT,
+   pid INTEGER PRIMARY KEY AUTOINCREMENT,
    Pname TEXT  NOT NULL,
    Age TEXT NOT NULL,
    Gender TEXT NOT NULL,
-   DateOfAdm TIMESTAMP NOT NULL default current_timestamp  
+   DateOfAdm TIMESTAMP NOT NULL default current_timestamp,
+   BedNo INTEGER NOT NULL,
+   diagnosis TEXT NOT NULL,
+   docname TEXT NOT NULL,
+   surgery TEXT NOT NULL 
 );
 
 
--- flaskr/views.py
+++ flaskr/views.py
@@ -12,7 +12,7 @@ bp = Blueprint('views', __name__)
 def index():
 	db = get_db()
 	posts = db.execute(
-		'SELECT p.id, Pname,Age,Gender,DateofAdm'
+		'SELECT p.pid, Pname,Age,Gender,DateofAdm'
 		' FROM patient p'
 		).fetchall()
 	return render_template('index.html', posts=posts)
@@ -23,6 +23,12 @@ def create():
         Pname = request.form['Pname']
         Age = request.form['Age']
         Gender = request.form['Gender']
+        BedNo=request.form['BedNo']
+        diagnosis=request.form['diagnosis']
+        docname=request.form['docname']
+        surgery=request.form['surgery']
+
+
         error = None
 
         if not Pname:
@@ -33,11 +39,36 @@ def create():
         else:
             db = get_db()
             db.execute(
-                'INSERT INTO patient (Pname,Age,Gender)'
-                ' VALUES (?, ?, ?)',
-                (Pname,Age,Gender)    
+                'INSERT INTO patient (Pname,Age,Gender,BedNo,diagnosis,docname,surgery)'
+                ' VALUES (?, ?, ?,?,?,?,?)',
+                (Pname,Age,Gender,BedNo,diagnosis,docname,surgery)    
             )
             db.commit()
             return redirect(url_for('views.index'))
 
-    return render_template('create.html')    
+    return render_template('create.html')
+
+@bp.route('/details/<int:pid>', methods=('GET', 'POST'))        
+def details(pid):
+	db=get_db()
+	posts=db.execute(
+    	'SELECT p.pid,BedNo'
+    	' FROM patient p'
+    	' WHERE p.pid={}'.format(pid)
+    	).fetchone()
+	Diagnosis=db.execute(
+    	'SELECT p.pid,diagnosis'
+    	' FROM patient p'
+    	' WHERE p.pid={}'.format(pid)
+    	).fetchone()
+	docname=db.execute(
+        'SELECT p.pid,docname'
+        ' FROM patient p'
+        ' WHERE p.pid={}'.format(pid)
+    	).fetchone()
+	surgery=db.execute(
+    	'SELECT q.pid,surgery'
+    	' FROM patient q'
+    	' WHERE q.pid={}'.format(pid)
+    	).fetchone()
+	return render_template('details.html', posts=posts,Diagnosis=Diagnosis,docname=docname,surgery=surgery)
